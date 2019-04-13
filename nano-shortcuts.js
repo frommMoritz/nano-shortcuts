a4e5d74f31905461dd1564655b64aa51a33e6455
Moritz Fromm <git@moritz-fromm.de>
2019-04-13 16:26:56 +0200

    Add option to close window (go to about:blank) by x
    - Raised version to v1.1
    - Close #2

--- nano-shortcuts.js
+++ nano-shortcuts.js
@@ -1,5 +1,3 @@
-console.debug("loaded");
-
 let keys = [
   { key: "h", path: "/" },
   { key: "g", path: "/download.php" },
@@ -9,27 +7,30 @@ let keys = [
   { key: "n", path: "/news.php" },
   { key: "w", path: "/who.php" },
   { key: "v", path: "/git.php" },
-  { key: "c", path: "/contact.php" }
+  { key: "c", path: "/contact.php" },
+  { key: "x", path: null }
 ];
 
-setTimeout(setShortcuts, 0);
+setShortcuts()
 
 function setShortcuts() {
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
     const shortcut = "shift+" + key.key;
-    console.debug(shortcut);
     addShortcut(shortcut, key);
     addShortcut(key.key, key);
   }
-  console.debug('all loaded');
 }
 
 function addShortcut(shortcut, key) {
   shortcuts.add(shortcut, function (event) {
-    const url = window.location.protocol + "//" + window.location.hostname + key.path;
-    window.focus();
-    console.debug(shortcut, url);
+    let url;
+    if (key.path === null)
+    {
+      url = 'about:blank';
+    } else {
+      url = window.location.protocol + "//" + window.location.hostname + key.path;
+    }
     window.location.assign(url);
     return false;
   });
